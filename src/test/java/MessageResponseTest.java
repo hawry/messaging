04a01be3c180d0018858309a7ed4134d9f156f27
hawry <hawry@hawry.net>
2018-06-28 17:07:16 +0200

    add response test cases

---
+++ src/test/java/MessageResponseTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.assertEquals;
+
+import com.google.gson.Gson;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import net.hawry.messaging.core.MessageResponse;
+
+
+public class MessageResponseTest {
+  Gson g = null;
+  @Before
+  public void setUp() {
+    g = new Gson();
+  }
+
+  @Test
+  public void testResponseSerialization() {
+    String json = "{\"recipient_id\": \"1254477777772919\",\"message_id\": \"mid.$cAAJsujCd2ORj_1qmrFdzhVa-4cvO\"}";
+    MessageResponse response = g.fromJson(json, MessageResponse.class);
+    assertEquals(response.getMessageId(), "mid.$cAAJsujCd2ORj_1qmrFdzhVa-4cvO");
+    assertEquals(response.getRecipientId(), "1254477777772919");
+  }
+}
