bbd7d0b92d1e6c890a6638b6c5cdf6fb2947534b
hawry <hawry@hawry.net>
2018-06-28 17:27:35 +0200

    remove user_ref and phone_nbr

--- src/main/java/net/hawry/messaging/core/MessageResponse.java
+++ src/main/java/net/hawry/messaging/core/MessageResponse.java
@@ -5,8 +5,6 @@ import com.google.gson.annotations.SerializedName;
 public class MessageResponse {
   @SerializedName("recipient_id") String recipientId;
   @SerializedName("message_id") String messageId;
-  @SerializedName("user_ref") String userRef;
-  @SerializedName("phone_number") String phoneNumber;
 
   /**
    * @return recipient id if hasRecipientId returns true, otherwise null
@@ -20,40 +18,5 @@ public class MessageResponse {
    */
   public String getMessageId() {
     return this.messageId;
-  }
-
-  /**
-   * @return true if recipient is identified by regular recipient id
-   */
-  public boolean hasRecipientId() {
-    return (this.recipientId != null);
-  }
-
-  /**
-   * @return true if recipient is identified by phone number
-   */
-  public boolean hasPhoneNumber() {
-    return (this.phoneNumber != null);
-  }
-
-  /**
-   * @return true if recipient is identified by user reference
-   */
-  public boolean hasUserRef() {
-    return (this.userRef != null);
-  }
-
-  /**
-   * @return user phone number if hasPhoneNumber returned true, otherwise null
-   */
-  public String getPhoneNumber() {
-    return this.phoneNumber;
-  }
-
-  /**
-   * @return user reference number if hasUserRef returned true, otherwise null
-   */
-  public String getUserRef() {
-    return this.userRef;
   }
 }
--- src/test/java/MessageResponseTest.java
+++ src/test/java/MessageResponseTest.java
@@ -1,4 +1,4 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import com.google.gson.Gson;
 
