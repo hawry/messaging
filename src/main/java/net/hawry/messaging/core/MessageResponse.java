bd60e1c9ea3204391e2e74e97dd4f2edc374f9be
hawry <hawry@hawry.net>
2018-06-28 17:24:07 +0200

    add docs

--- src/main/java/net/hawry/messaging/core/MessageResponse.java
+++ src/main/java/net/hawry/messaging/core/MessageResponse.java
@@ -5,9 +5,11 @@ import com.google.gson.annotations.SerializedName;
 public class MessageResponse {
   @SerializedName("recipient_id") String recipientId;
   @SerializedName("message_id") String messageId;
+  @SerializedName("user_ref") String userRef;
+  @SerializedName("phone_number") String phoneNumber;
 
   /**
-   * @return recipient id
+   * @return recipient id if hasRecipientId returns true, otherwise null
    */
   public String getRecipientId() {
     return this.recipientId;
@@ -21,9 +23,37 @@ public class MessageResponse {
   }
 
   /**
-   * @return true if response contains both a message id and recipient id
+   * @return true if recipient is identified by regular recipient id
    */
-  public boolean isValid() {
-    return this.recipientId.length() > 0 && this.messageId.length() > 0;
+  public boolean hasRecipientId() {
+    return (this.recipientId != null);
+  }
+
+  /**
+   * @return true if recipient is identified by phone number
+   */
+  public boolean hasPhoneNumber() {
+    return (this.phoneNumber != null);
+  }
+
+  /**
+   * @return true if recipient is identified by user reference
+   */
+  public boolean hasUserRef() {
+    return (this.userRef != null);
+  }
+
+  /**
+   * @return user phone number if hasPhoneNumber returned true, otherwise null
+   */
+  public String getPhoneNumber() {
+    return this.phoneNumber;
+  }
+
+  /**
+   * @return user reference number if hasUserRef returned true, otherwise null
+   */
+  public String getUserRef() {
+    return this.userRef;
   }
 }
