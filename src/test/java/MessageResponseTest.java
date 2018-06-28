221e8372f3dd06cc6ce262d00389b0c1672a2983
hawry <hawry@hawry.net>
2018-06-28 22:54:08 +0200

    add error tests

--- src/test/java/MessageResponseTest.java
+++ src/test/java/MessageResponseTest.java
@@ -2,16 +2,18 @@ import static org.junit.Assert.*;
 
 import com.google.gson.Gson;
 
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import net.hawry.messaging.core.response.MessageResponse;
 
 
 public class MessageResponseTest {
-  Gson g = null;
-  @Before
-  public void setUp() {
+  
+  static Gson g = null;
+
+  @BeforeClass
+  public static void setUpBeforeClass() {
     g = new Gson();
   }
 
@@ -21,5 +23,25 @@ public class MessageResponseTest {
     MessageResponse response = g.fromJson(json, MessageResponse.class);
     assertEquals(response.getMessageId(), "mid.$cAAJsujCd2ORj_1qmrFdzhVa-4cvO");
     assertEquals(response.getRecipientId(), "1254477777772919");
+  }
+
+  public static class ErrorResponses {
+
+    @Test
+    public void testHasErrors() {
+      Gson g = new Gson();
+
+      MessageResponse response = g.fromJson("{\"error\": {\"message\": \"Invalid OAuth access token.\",\"type\": \"OAuthException\",\"code\": 190,\"error_subcode\":1234567,\"fbtrace_id\": \"BLBz/WZt8dN\"}}", MessageResponse.class);
+
+      assertTrue(response.hasErrors());
+      assertNull(response.getMessageId());
+      assertNull(response.getRecipientId());
+
+      assertEquals("Invalid OAuth access token.", response.getError().getErrorMessage());
+      assertEquals(190,response.getError().getErrorCode());
+      assertEquals(1234567,response.getError().getErrorSubcode());
+      assertEquals("BLBz/WZt8dN", response.getError().getTraceId());
+      assertEquals("Access Token Error", response.getError().getErrorCategory());
+    }
   }
 }
