47fcebd1d81e09111fc9d584d13952a608108ae7
hawry <hawry@hawry.net>
2018-07-11 20:30:52 +0200

    create messageresponse exception tests

--- src/test/java/core/response/MessageResponseTest.java
+++ src/test/java/core/response/MessageResponseTest.java
@@ -1,14 +1,21 @@
 package core.response;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
 import com.google.gson.Gson;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import net.hawry.messaging.core.response.MessageResponse;
+import net.hawry.messaging.exceptions.InvalidJsonException;
 
 @DisplayName("MessageResponse tests")
 class MessageResponseTest {
@@ -38,5 +45,43 @@ class MessageResponseTest {
   @DisplayName("returns no errors")
   void noErrors() {
     assertFalse(mr.hasErrors());
+  }
+
+  @Test
+  @DisplayName("message response fromJson returns a valid instance")
+  void fromJson_Valid() {
+    String json = "{'recipient_id': '123','message_id':'$mid.id'}";
+    MessageResponse rsp = null;
+    
+    try {
+      rsp = MessageResponse.fromJson(json);
+    } catch (InvalidJsonException e) {
+      Assertions.fail("exception thrown: " + e.getMessage());
+    }
+
+    assertEquals("$mid.id", rsp.getMessageId());
+    assertEquals("123", rsp.getRecipientId());
+  }
+
+  @Test
+  @DisplayName("message response fromJson returns a valid error")
+  void fromJson_ValidError() {
+    String json = "{'error':{'code':100}}";
+
+    MessageResponse rsp = null;
+    try {
+      rsp = MessageResponse.fromJson(json);
+    } catch (InvalidJsonException e) {
+      Assertions.fail("exception thrown: " + e.getMessage());
+    }
+    assertTrue(rsp.hasErrors());
+    assertNotNull(rsp.getError());
+  }
+
+  @Test
+  @DisplayName("invalid json throws exception")
+  void fromJson_InvalidJson() {
+    String json = "{'recipient_id':123";
+    assertThrows(InvalidJsonException.class, () -> { MessageResponse.fromJson(json); });
   }
 }
