3132e77f0c7c0eebbf3551501a1e8037c9fe14ee
Tomas <Hawry@users.noreply.github.com>
2018-07-11 18:59:20 +0200

    Remove unused imports (#15)
    * remove unused imports
    
    * enable jacoco
    
    * Revert "enable jacoco"
    
    This reverts commit 46f04f11904ddf6d75fce3ec0af29751099676ce.

--- src/test/java/core/MessageTest.java
+++ src/test/java/core/MessageTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import net.hawry.messaging.core.Content;
