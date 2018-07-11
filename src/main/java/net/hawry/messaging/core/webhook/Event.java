a254f65e9990993e1103fe7b6e76b70edcac71d1
hawry <hawry@hawry.net>
2018-07-11 20:45:43 +0200

    add fromJson methods and test cases

--- src/main/java/net/hawry/messaging/core/webhook/Event.java
+++ src/main/java/net/hawry/messaging/core/webhook/Event.java
@@ -1,6 +1,10 @@
 package net.hawry.messaging.core.webhook;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.annotations.SerializedName;
+
+import net.hawry.messaging.exceptions.InvalidJsonException;
 
 public class Event {
   @SerializedName("object") String object;
@@ -12,5 +16,16 @@ public class Event {
 
   public Entry[] getEntries() {
     return this.entry;
+  }
+
+  public static Event fromJson(String json) throws InvalidJsonException {
+    Gson g = new Gson();
+    Event event = null;
+    try {
+      event = g.fromJson(json, Event.class);
+    } catch (JsonSyntaxException e) {
+      throw new InvalidJsonException(Event.class.getSimpleName());
+    }
+    return event;
   }
 }
--- src/test/java/core/webhook/EventTest.java
+++ src/test/java/core/webhook/EventTest.java
@@ -1,14 +1,17 @@
 package core.webhook;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import com.google.gson.Gson;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import net.hawry.messaging.core.webhook.Event;
+import net.hawry.messaging.exceptions.InvalidJsonException;
 
 @DisplayName("Event")
 class EventTest {
@@ -32,5 +35,28 @@ class EventTest {
   @DisplayName("returns entry array")
   void validEntryArray() {
     assertEquals(2, event.getEntries().length);
+  }
+
+  @Test
+  @DisplayName("returns valid Event object from valid json")
+  void fromJson_ValidData() {
+    String json = "{'object':'page', 'entry': [{'id':'123','time':12344444,'messaging': []}]}";
+    Event event = null;
+
+    try {
+      event = Event.fromJson(json);
+    } catch (InvalidJsonException e) {
+      Assertions.fail("exception thrown: " + e.getMessage());
+    }
+
+    assertEquals("page", event.getObject());
+    assertEquals(1, event.getEntries().length);
+  }
+
+  @Test
+  @DisplayName("throws exception if invalid JSON")
+  void fromJson_InvalidData() {
+    String json = "{'object':'page' 'entry': [{'id':'123','time':12344444,'messaging': []}]}";
+    assertThrows(InvalidJsonException.class, () -> { Event.fromJson(json); });
   }
 }
